49a3ece6fa630e263fa8d387873035f19b04cbff
plmrr <aquaossa@gmail.com>
2024-10-29 19:30:35 +0100

    Add activation functions with derivatives

---
+++ utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+def linear(x):
+    return x
+
+def linear_derivative(x):
+    return np.ones_like(x)
+
+def relu(x):
+    return np.maximum(0, x)
+
+def relu_derivative(x):
+    return np.where(x > 0, 1, 0)
+
+def leaky_relu(x, alpha=0.01):
+    return np.where(x > 0, x, alpha * x)
+
+def leaky_relu_derivative(x, alpha=0.01):
+    return np.where(x > 0, 1, alpha)
