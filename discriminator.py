3dfd3a600ba97093beb98e86493f90114b28bcd6
plmrr <aquaossa@gmail.com>
2024-10-29 19:34:50 +0100

    Class placeholders

---
+++ discriminator.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Discriminator:
+    def __init__(self):
+        # weights placeholder
+        pass
+
+    def forward(self):
+        # placeholder
+        pass
+
+    def backward(self):
+        # placeholder
+        pass
---
+++ generator.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Generator:
+    def __init__(self):
+        # weights placeholder
+        pass
+
+    def forward(self):
+        # placeholder
+        pass
+
+    def backward(self):
+        # placeholder
+        pass
