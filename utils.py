0fd58762b68709f883206f0959e143bc87ad6a88
plmrr <aquaossa@gmail.com>
2024-11-12 16:10:07 +0100

    Add tanh activation fun. & binary cross entropy

--- utils.py
+++ utils.py
@@ -7,12 +7,6 @@ def sigmoid_derivative(x):
     s = sigmoid(x)
     return s * (1 - s)
 
-def linear(x):
-    return x
-
-def linear_derivative(x):
-    return np.ones_like(x)
-
 def relu(x):
     return np.maximum(0, x)
 
@@ -24,3 +18,22 @@ def leaky_relu(x, alpha=0.01):
 
 def leaky_relu_derivative(x, alpha=0.01):
     return np.where(x > 0, 1, alpha)
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_derivative(x):
+    return 1 - x ** 2
+
+def binary_cross_entropy(y_pred, y_true):
+    epsilon = 1e-12
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+def binary_cross_entropy_derivative(y_pred, y_true):
+    epsilon = 1e-12
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return (y_pred - y_true) / (y_pred * (1 - y_pred))
+
+def generate_noise(a, b, shape):
+    return np.random.normal(a, b, size=shape)
