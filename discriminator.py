5afa938346ed514b84652edd40966c1f5431c725
plmrr <aquaossa@gmail.com>
2024-11-12 17:14:59 +0100

    Add forward step for generator and discriminator

--- discriminator.py
+++ discriminator.py
@@ -1,20 +1,29 @@
 import numpy as np
-from utils import initialize_weights
+from utils import initialize_weights, leaky_relu, sigmoid
 
 class Discriminator:
-    def __init__(self, input_size, hidden_sizes, output_size):
+    def __init__(self, input_size, hidden_sizes):
         self.weights = []
         self.biases = []
-        layers_sizes = [input_size] + hidden_sizes + [output_size]
-        for i in range(len(layers_sizes)-1):
-            w = initialize_weights((layers_sizes[i], layers_sizes[i+1]))
-            b = np.zeros((1, layers_sizes[i+1]))
+        layers_sizes = [input_size] + hidden_sizes + [1] # output is 1 value
+        for i in range(len(layers_sizes) - 1):
+            w = initialize_weights((layers_sizes[i], layers_sizes[i + 1]))
+            b = np.zeros((1, layers_sizes[i + 1]))
             self.weights.append(w)
             self.biases.append(b)
 
-    def forward(self):
-        # placeholder
-        pass
+    def forward(self, x):
+        self.activations = [x]
+        x = x.reshape(x.shape[0], -1)
+        for i in range(len(self.weights) - 1):
+            x = np.dot(x, self.weights[i]) + self.biases[i]
+            x = leaky_relu(x)
+            self.activations.append(x)
+        
+        x = np.dot(x, self.weights[-1]) + self.biases[-1]
+        x = sigmoid(x)
+        self.activations.append(x)
+        return x
 
     def backward(self):
         # placeholder
--- generator.py
+++ generator.py
@@ -1,20 +1,29 @@
 import numpy as np
-from utils import initialize_weights
+from utils import initialize_weights, leaky_relu, tanh
 
 class Generator:
     def __init__(self, input_size, hidden_sizes, output_size):
         self.weights = []
         self.biases = []
         layers_sizes = [input_size] + hidden_sizes + [output_size]
-        for i in range(len(layers_sizes)-1):
-            w = initialize_weights((layers_sizes[i], layers_sizes[i+1]))
-            b = np.zeros((1, layers_sizes[i+1]))
+        for i in range(len(layers_sizes) - 1):
+            w = initialize_weights((layers_sizes[i], layers_sizes[i + 1]))
+            b = np.zeros((1, layers_sizes[i + 1]))
             self.weights.append(w)
             self.biases.append(b)
 
-    def forward(self):
-        # placeholder
-        pass
+    def forward(self, z):
+        self.activations = [z]
+        x = z
+        for i in range(len(self.weights) - 1):
+            x = np.dot(x, self.weights[i]) + self.biases[i]
+            x = leaky_relu(x)
+            self.activations.append(x)
+        
+        x = np.dot(x, self.weights[-1]) + self.biases[-1]
+        x = tanh(x)
+        self.activations.append(x)
+        return x
 
     def backward(self):
         # placeholder
