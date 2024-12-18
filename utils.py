6502f4a506ec6c839ae6dd645130a4125eee7d25
plmrr <aquaossa@gmail.com>
2024-12-18 19:17:02 +0100

    Update utils

--- utils.py
+++ utils.py
@@ -11,8 +11,6 @@ def relu(x):
     return np.maximum(0, x)
 
 def relu_derivative(x):
-    # x - preaktywacje
-    # d/dx relu(x) = 1 jeśli x>0, else 0
     return (x > 0).astype(x.dtype)
 
 def leaky_relu(x, alpha=0.02):
@@ -29,50 +27,11 @@ def tanh_derivative(x):
 
 def binary_cross_entropy(y_pred, y_true):
     epsilon = 1e-8
-    # y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
     return -np.mean(y_true * np.log(y_pred + epsilon) + (1 - y_true) * np.log(1 - y_pred + epsilon))
-    # return -np.mean(y_true * np.log(y_pred + epsilon) + (1 - y_true) * np.log(1 - y_pred + epsilon))
 
 def binary_cross_entropy_derivative(y_pred, y_true):
     epsilon = 1e-8
-    # y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
     return (y_pred - y_true) / ((y_pred * (1 - y_pred)) + epsilon)
-    # return (y_pred - y_true) / ((y_pred * (1 - y_pred)) + epsilon)
-
-def wasserstein_loss_disc(real_output, fake_output):
-    # Loss for the discriminator
-    return -np.mean(real_output) + np.mean(fake_output)
-
-def wasserstein_loss_gen(fake_output):
-    # Loss for the generator
-    return -np.mean(fake_output)
-
-def wasserstein_loss_disc_derivative(real_output, fake_output):
-    # Gradienty dla rzeczywistych i fałszywych danych
-    grad_real = -np.ones_like(real_output)
-    grad_fake = np.ones_like(fake_output)
-    return grad_real, grad_fake
-
-def wasserstein_loss_gen_derivative(fake_output):
-    # Gradient dla generatora
-    return -np.ones_like(fake_output)
-
-def compute_gradient_penalty(discriminator, real_samples, fake_samples, lambda_gp=10):
-    # Interpolate between real and fake samples
-    alpha = np.random.uniform(0, 1, size=real_samples.shape)
-    interpolates = alpha * real_samples + (1 - alpha) * fake_samples
-
-    # Forward pass through the discriminator
-    interpolates_output = discriminator.forward(interpolates)
-
-    # Compute gradients with respect to inputs
-    gradients = np.gradient(interpolates_output, interpolates, edge_order=2)
-    gradient_norms = np.sqrt(np.sum(gradients**2, axis=(1, 2, 3)))
-
-    # Gradient penalty
-    gradient_penalty = np.mean((gradient_norms - 1)**2)
-    return lambda_gp * gradient_penalty
-
 
 def initialize_weights(shape):
     return np.random.randn(*shape) * 0.01
